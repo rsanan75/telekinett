88d0e238a1f343b6e6529aaff7c4d38427d9e30b
Typografik <pgulia@payals-air.lan>
2024-02-03 21:59:12 -0500

    DB update

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'telekinett_prod' );
 
 /** Database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'telekinett_user' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', 'L^4B$r!6Bb$XUs6XI^#U' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '162.240.167.173' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
